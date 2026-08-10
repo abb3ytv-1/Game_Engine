32806c18b1a45955baa318db8a387419b94f76f8
Abbey Kelley <abbey.k.040104@gmail.com>
2026-08-10 10:09:52 -0600

    game data file read/write

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,65 +1,97 @@
 #include "../Engine/Engine.h"
+#include "Space_Game.h"
+
+#include <fstream>
 #include <iostream>
 #include <memory>
-#include "Space_Game.h"
 
 using namespace nu;
 
-class Object {
-public:
-	Object() { std::cout << "constructor\n"; }
-	~Object() { std::cout << "destructor\n"; }
-
-	Object(const Object& object) { std::cout << "copy\n"; }
-	Object& operator = (const Object& object) { std::cout << "assignment\n"; return *this; }
-
-};
-
 int main() {
-	//std::cout << "====================== Object ==================\n"; {
-	//	Object objectA;
-	//	Object objectB(objectA);
-	//	Object objectC;
-	//	objectC = objectA;
-	//}
 
-	//std::cout << "===================== Raw Pointers =====================\n"; {
-	//	Object* objectA = new Object();
-	//	std::cout << objectA << std::endl;
-	//	Object* objectB = new Object(*objectA);
-	//	std::cout << objectB << std::endl;
-	//	Object* objectC = nullptr;
-	//	objectC = objectA;
-	//	std::cout << objectC << std::endl;
+	// read file (input file)
+	{
+		std::ifstream inFile("Assets/Data/data.txt");
+		if (inFile.is_open()) {
+			std::string str;
+			while (std::getline(inFile, str)) {
+				std::cout << str << std::endl;
+			}
+		}
+		else {
+			std::cout << "could not load: Assets/Data/data.txt\n";
+		}
+	}
 
-	//	delete objectA;
-	//	delete objectB;
-	//}
+	// write file (output file)
+	{
+		std::ofstream outFile("Assets/Data/data.txt", std::ios::app);
+		if (outFile.is_open()) {
+			outFile << "\nI want to buy a new coffee machine.\n";
+		}
+	}
 
-	//std::cout << "===================== Smart Pointers =====================\n"; {
-	//	std::unique_ptr<Object> objectA = std::make_unique<Object>();
-	//	std::cout << objectA.get() << std::endl;
-	//	std::unique_ptr<Object> objectB;
-	//	objectB = std::move(objectA);
-	//	std::cout << objectB.get() << std::endl;
-	//}
+	// read/write (input/output)
+	{
+		std::fstream ioFile("Assets/Data/data.txt", std::ios::in | std::ios::out | std::ios::app);
+		if (ioFile.is_open()) {
+			// input
+			ioFile << "Add a line.\n";
+			ioFile.seekg(0);
+			// output
+			std::string str;
+			while(std::getline(ioFile, str)) {
+				std::cout << str << std::endl;
+			}
+		}
+	}
 
-	//std::cout << " ===================== Unique Pointers ====================\n"; {
-	//	std::unique_ptr<Object> objectA = std::make_unique<Object>();
-	//	std::cout << objectA.get() << std::endl;
-	//	
-	//}
+	{ 
+		std::string name;
+		int score;
+		bool isAlive;
+		
+		//save game data
+		bool save = false;
+		if (save) {
+			name = "Abbey";
+			score = 143;
+			isAlive = true;
 
-	//std::cout << " ===================== Shared Pointers ====================\n"; {
-	//	auto objectA = std::make_shared<Object>();
-	//	std::cout << objectA.get() << std::endl;
-	//	std::cout << objectA.use_count() << std::endl;
-	//	auto objectB = objectA;
+			// save game data
+			std::ofstream file("Assets/Data/game.txt", std::ios::app);
+			if (file.is_open()) {
+				file << name << "\n";
+				file << score << "\n";
+				file << std::boolalpha << isAlive << "\n";
+			}
+		}
+
+		//load game data
+		bool load = true;
+		if (load) {
+			//load game
+			std::ifstream file("Assets/Data/data.txt");
+			if (file.is_open()) {
+				std::getline(file, name);
+
+				std::string str;
+				std::getline(file, str);
+
+				file >> score;
+				file >> std::boolalpha >> isAlive;
+
+			}
+		}
+
+		//display game data
+		std::cout << name << "\n";
+		std::cout << score << "\n";
+		std::cout << std::boolalpha << isAlive << "\n";
+	}
 
 
-
-	//}
-
+	return 0;
 
 	if (!engine.Initialize()) {
 		return 1;
